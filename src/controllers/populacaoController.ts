e5d04fa4a28f6cd6f718390440372eae87cc94b4
mathtml <matheusfgoncalves12@gmail.com>
2025-01-07 16:34:51 -0300

    atualização e criação de paginação.

--- src/controllers/populacaoController.ts
+++ src/controllers/populacaoController.ts
@@ -1,16 +1,39 @@
-import { Request, Response } from 'express';
-import { getPopulacao } from '../services/populacaoService';
+import { Request, Response } from "express";
+import {
+  getPopulacao,
+  getPopulacaoComPaginacao,
+} from "../services/populacaoService";
 
 export const getAllPopulacao = async (req: Request, res: Response) => {
-    try {
-      const populacao = await getPopulacao();
-      res.status(200).json(populacao);
-    } catch (error) {
-      if (error instanceof Error) {
-        res.status(500).json({ error: error.message });
-      } else {
-        res.status(500).json({ error: 'Erro desconhecido' });
-      }
+  try {
+    const populacao = await getPopulacao();
+    res.status(200).json(populacao);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: "Erro desconhecido" });
     }
-  };
-  
+  }
+};
+
+export const getAllPopulacaoComPaginacao = async (req: Request, res: Response) => {
+  try {
+    const page = parseInt(req.query.page as string, 10) || 1;
+    const limit = parseInt(req.query.limit as string, 10) || 10;
+    const populacao = await getPopulacaoComPaginacao(page, limit);
+    res.status(200).json({
+      pagina: populacao.page,
+      limit: populacao.limit,
+      total: populacao.total,
+      totalPages: populacao.totalPages,
+      dados: populacao.dados,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: "Erro ao fazer consulta." });
+    }
+  }
+};
--- src/services/populacaoService.ts
+++ src/services/populacaoService.ts
@@ -1,23 +1,57 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export const getPopulacao = async () => {
-    try {
-      return await prisma.populacaoMunic.findMany({
-        select: {
-          uf: true,
-          codUf: true,
-          codMunic: true,
-          nomeMunicipio: true,
-          populacao: true,
-        },
-      }); 
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(`Erro ao buscar dados da população: ${error.message}`);
-      }
-      throw new Error('Erro desconhecido ao buscar dados da população');
+  try {
+    return await prisma.populacaoMunic.findMany({
+      select: {
+        uf: true,
+        codUf: true,
+        codMunic: true,
+        nomeMunicipio: true,
+        populacao: true,
+      },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Erro ao buscar dados da população: ${error.message}`);
     }
-  };
+    throw new Error("Erro desconhecido ao buscar dados da população");
+  }
+};
+
+export const getPopulacaoComPaginacao = async (page: number, limit: number = 10) => {
+  try {
+    const skip = (page - 1) * limit;  // Ignora os registros das páginas anteriores
+    const populacao = await prisma.populacaoMunic.findMany({
+      skip,  // Usa o cálculo de skip
+      take: limit,  // Retorna apenas os registros dessa página
+      select: {
+        uf: true,
+        codUf: true,
+        codMunic: true,
+        nomeMunicipio: true,
+        populacao: true,
+      },
+    });
+
+    // Conta o total de registros para calcular o número total de páginas
+    const total = await prisma.populacaoMunic.count();
+
+    // Retorna a página atual, o limite, total de itens e as páginas totais
+    return {
+      page,
+      limit,
+      total,
+      totalPages: Math.ceil(total / limit),  // Calcula o número total de páginas
+      dados: populacao,
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Erro ao buscar dados da população: ${error.message}`);
+    }
+    throw new Error("Erro desconhecido ao buscar dados da população");
+  }
+};
 
